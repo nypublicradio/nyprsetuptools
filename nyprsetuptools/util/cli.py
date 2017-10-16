8ae38d96d3deb9786c99ef28f2ba08e793f41bd7
Matt Oberle <matt.r.oberle@gmail.com>
2017-10-16 11:42:22 -0400

    exposes nyprsetuptools commands to the cli independent of setuptools

---
+++ nyprsetuptools/util/cli.py
@@ -0,0 +1,55 @@
+import sys
+from argparse import ArgumentParser, SUPPRESS
+from setuptools.dist import Distribution
+
+from nyprsetuptools import DockerDeploy
+
+
+def make_command(CommandClass):
+    dist = Distribution()
+    cmd = CommandClass(dist)
+
+    for long_opt, short_opt, help_text in cmd.user_options:
+        if long_opt.endswith('='):
+            action = 'store'
+            default = None
+        else:
+            action = 'store_true'
+            default = False
+        long_opt = '--{}'.format(long_opt.rstrip('='))
+
+        if short_opt:
+            short_opt = '-{}'.format(short_opt)
+
+        opts = filter(None, [short_opt, long_opt])
+        yield (opts, help_text, action, default)
+
+
+def make_cli(command_classes):
+    parser = ArgumentParser()
+    subparsers = parser.add_subparsers()
+    for cls in command_classes:
+        subparser = subparsers.add_parser(cls.__name__)
+        subparser.add_argument('--cls', help=SUPPRESS, default=cls)
+        for opts, help_text, action, default in make_command(cls):
+            subparser.add_argument(*opts, help=help_text, action=action,
+                                   default=default)
+    args = parser.parse_args()
+    if not args.__dict__:
+        sys.exit(parser.print_help())
+    return args.__dict__
+
+
+def run_cmd():
+    classes = [DockerDeploy]
+    args = make_cli(classes)
+    dist = Distribution()
+    cls = args.pop('cls')
+    cmd = cls(dist)
+
+    cmd.initialize_options()
+    for key, value in args.items():
+        if value:
+            setattr(cmd, key, value)
+    cmd.finalize_options()
+    cmd.run()
