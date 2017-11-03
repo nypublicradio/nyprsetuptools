7128e5ca106acbef552e9ec195b4e6824f645acd
Matt Oberle <matt.r.oberle@gmail.com>
2017-11-03 11:29:40 -0400

    adds setup_keywords function for distutils extensions

--- nyprsetuptools/__init__.py
+++ nyprsetuptools/__init__.py
@@ -1,3 +1,4 @@
 from nyprsetuptools.commands.build import InstallRequirements, InstallTestRequirements
 from nyprsetuptools.commands.test import DjangoTest, PyTest, PyTestDistributed
 from nyprsetuptools.commands.deploy import LambdaDeploy, DockerDeploy
+from nyprsetuptools.setup_keywords import setup_keywords
---
+++ nyprsetuptools/setup_keywords.py
@@ -0,0 +1,9 @@
+def setup_keywords(dist, attr, value):
+    """
+    This is a validation function that always
+    allows attributes to be passed to setup.py
+    for nyprsetuptools extensions.
+    Each distutils.command extension requires
+    a distutils.setup_keywords component.
+    """
+    pass
