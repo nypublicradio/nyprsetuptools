5e3eb3a4c0a0273b918e1c8cfb5c9fefd6731c0c
Nico Castro <ncastro@nypublicradio.org>
2021-11-18 15:52:21 -0500

    adding a timeout

--- nyprsetuptools/commands/deploy.py
+++ nyprsetuptools/commands/deploy.py
@@ -595,9 +595,13 @@ class LambdaDeploy(Command):
             # update call is still running, the build will fail and the env vars won't be
             # updated. So we need to wait until the LastUpdateStatus of the function is "Successful" instead
             # of "InProgress".
-            while client.get_function(FunctionName=function_name)['Configuration']['LastUpdateStatus'] == 'InProgress':
-                print('Waiting for code deploy before updating env vars.')
+            timeout = 0
+            while timeout < 60 and client.get_function(FunctionName=function_name)['Configuration']['LastUpdateStatus'] == 'InProgress':
+                print(f'Waiting for code deploy before updating env vars. {60 - timeout} seconds until timeout.')
                 time.sleep(5)
+                timeout += 5
+            if timeout == 60:
+                sys.exit(f"The update to {function_name} has timed out before updating the Env Vars") 
             client.update_function_configuration(
                 FunctionName=function_name,
                 Handler=function_handler,
